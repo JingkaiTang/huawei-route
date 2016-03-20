0e55d7c30544d087734ffc7b0b2ba92916bb7f48
Jingkai Tang <jingkaitang@gmail.com>
2016-03-20 14:27:49 +0800

    updated: loop args for test script

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-printf 'Test Case => '$1':\n'
-printf '    Used: %s s\n' `(/usr/bin/time -f '%e' bash run.sh $1 > /dev/null) 2>&1`
-printf '    Cost: %s\n' `bash check.sh $1`
+for arg in $*
+do
+  printf 'Test Case => '$arg':\n'
+  printf '    Used: %s s\n' `(/usr/bin/time -f '%e' bash run.sh $arg > /dev/null) 2>&1`
+  printf '    Cost: %s\n' `bash check.sh $arg`
+done
