181a5b447e4e59e61c59aa4c9601759ff589de46
Jingkai Tang <jingkaitang@gmail.com>
2016-03-20 14:01:32 +0800

    fixed: ms => s

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 printf 'Test Case => '$1':\n'
-printf '    Used: %s ms\n' `(/usr/bin/time -f '%e' bash run.sh $1 > /dev/null) 2>&1`
+printf '    Used: %s s\n' `(/usr/bin/time -f '%e' bash run.sh $1 > /dev/null) 2>&1`
 printf '    Cost: %s\n' `bash check.sh $1`
