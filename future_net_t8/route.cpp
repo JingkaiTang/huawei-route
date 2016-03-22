fa46014d03d3a08df86b58ae82163d5606277202
Jingkai Tang <jingkaitang@gmail.com>
2016-03-22 23:22:17 +0800

    updated: extend bad nodes

--- future_net_t8/route.cpp
+++ future_net_t8/route.cpp
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#define FOUND 0
+#define NOT_FOUND 1
+#define BAD_NODE 2
+
 TopoNode *topo = NULL;
 DemandSet *demand = NULL;
 int *path = NULL;
@@ -13,49 +17,67 @@ Bitmap *path_bitmap = NULL;
 Bitmap *bad_nodes = NULL;
 
 
-bool search(int pass_count, int node) {
+int search(int pass_count, int node) {
   path_bitmap->set(node);
   if (demand->bitmap->test(node)) {
     pass_count ++;
   }
 
   if (pass_count == demand->pass_size && node == demand->end) {
-    return true;
+    return FOUND;
   }
 
-  if (!bad_nodes->test(node)) {
-    TopoNode *cur_node = &topo[node];
+  if (bad_nodes->test(node)) {
+    path_bitmap->unset(node);
+    return BAD_NODE;
+  }
 
-    if (cur_node->out_degree == 0) {
-      if (demand->bitmap->test(node)) {
-        path_size = 0;
-        return true;
-      }
-      bad_nodes->set(node);
-    } else {
+  TopoNode *cur_node = &topo[node];
 
-      TopoArrow *cur_arrow;
-      for (int i = 0; i < cur_node->out_degree; i++) {
-        cur_arrow = &cur_node->arrows[i];
-
-        if (path_bitmap->test(cur_arrow->target)) {
-          continue;
-        }
-
-        path[path_size] = cur_arrow->number;
-        path_size++;
-
-        if (search(pass_count, cur_arrow->target)) {
-          return true;
-        }
-
-        path_size--;
-      }
+  if (cur_node->out_degree == 0 && node != demand->end) {
+    if (demand->bitmap->test(node)) {
+      path_size = 0;
+      return FOUND;
     }
+
+    bad_nodes->set(node);
+    path_bitmap->unset(node);
+    return BAD_NODE;
+  }
+
+  TopoArrow *cur_arrow;
+  bool is_bad_node = true;
+  for (int i = 0; i < cur_node->out_degree; i++) {
+    cur_arrow = &cur_node->arrows[i];
+
+    if (path_bitmap->test(cur_arrow->target)) {
+      is_bad_node = false;
+      continue;
+    }
+
+    path[path_size] = cur_arrow->number;
+    path_size++;
+
+    int flag = search(pass_count, cur_arrow->target);
+
+    if (flag == FOUND) {
+      return FOUND;
+    }
+
+    if (flag != BAD_NODE) {
+      is_bad_node = false;
+    }
+
+    path_size--;
   }
 
   path_bitmap->unset(node);
-  return false;
+
+  if (is_bad_node) {
+    bad_nodes->set(node);
+    return BAD_NODE;
+  }
+  return NOT_FOUND;
 }
 
 void search_route(TopoNode *t, int node_scope, DemandSet *d) {
