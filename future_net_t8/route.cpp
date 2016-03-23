ed8be39671da69910232c2c699aa0d8686cb2022
Jingkai Tang <jingkaitang@gmail.com>
2016-03-23 10:50:26 +0800

    added: dangling nodes

--- future_net_t8/route.cpp
+++ future_net_t8/route.cpp
@@ -5,17 +5,20 @@
 #define FOUND 0
 #define NOT_FOUND 1
 #define BAD_NODE 2
+#define GOOD_NODE 3
 
 TopoNode *topo = NULL;
 DemandSet *demand = NULL;
 int *path = NULL;
 int path_size = 0;
 Bitmap *path_bitmap = NULL;
+Bitmap *good_nodes = NULL;
+TopoNode *dangling = NULL;
 
 int search(int pass_count, int node) {
   path_bitmap->set(node);
   if (demand->bitmap->test(node)) {
-    pass_count ++;
+    pass_count++;
   }
 
   if (pass_count == demand->pass_size && node == demand->end) {
@@ -28,6 +31,18 @@ int search(int pass_count, int node) {
     cur_arrow = &cur_node->arrows[i];
 
     if (path_bitmap->test(cur_arrow->target)) {
+      bool is_exist = false;
+      for (int j = 0; j < dangling[node].out_degree; j++) {
+        if (dangling[node].arrows[j].target == cur_arrow->target) {
+          is_exist = true;
+        }
+      }
+
+      if (!is_exist) {
+        dangling[node].arrows[dangling[node].out_degree] = *cur_arrow;
+        dangling[node].out_degree++;
+      }
+
       continue;
     }
 
@@ -38,14 +53,14 @@ int search(int pass_count, int node) {
 
     if (flag == FOUND) {
       return FOUND;
-    }
-
-    if (flag == BAD_NODE) {
-      if (i < cur_node->out_degree-1) {
-        cur_arrow[i] = cur_arrow[cur_node->out_degree-1];
+    } else if (flag == BAD_NODE) {
+      if (i < cur_node->out_degree - 1) {
+        cur_arrow[i] = cur_arrow[cur_node->out_degree - 1];
         i--;
       }
-      cur_node->out_degree --;
+      cur_node->out_degree--;
+    } else if (flag == GOOD_NODE) {
+      good_nodes->set(node);
     }
 
     path_size--;
@@ -61,6 +76,10 @@ int search(int pass_count, int node) {
     return BAD_NODE;
   }
 
+  if (good_nodes->test(node) || node == demand->end) {
+    return GOOD_NODE;
+  }
+
   return NOT_FOUND;
 }
 
@@ -70,16 +89,20 @@ void search_route(TopoNode *t, int node_scope, DemandSet *d) {
   path = new int[node_scope];
   path_size = 0;
   path_bitmap = new Bitmap(node_scope);
+  good_nodes = new Bitmap(node_scope);
+  TopoNode __dangling[600];
+  dangling = __dangling;
 
   LOG("DEMAND => ");
   BITMAP_SHOW(demand->bitmap);
 
   search(0, demand->start);
 
-  for (int i = 0; i < path_size; i ++) {
+  for (int i = 0; i < path_size; i++) {
     record_result(path[i]);
   }
 
   delete[] path;
   delete path_bitmap;
+  delete good_nodes;
 }
